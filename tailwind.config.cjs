0a3b29f3a08c6c1c3ead0df3457be8bd6f1a5970
Andrelino Silva <andrelinodev@gmail.com>
2022-09-19 17:06:08 -0300

    :alarm_clock:

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,9 +12,9 @@ module.exports = {
       backgroundImage: {
         // prettier-ignore
         // eslint-disable-next-line quote-props, quotes
-        'galaxy': "url('./assets/background-galaxy.png')",
+        galaxy: "url('/assets/background-galaxy.png')",
         'nlw-gradient':
-          'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 40.57%)',
+          'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
         'game-gradient':
           'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       },

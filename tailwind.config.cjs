5bd71b1d513ea571cc8eaf77e9f19796ce8835e8
Andrelino Silva <andrelinodev@gmail.com>
2022-09-19 16:27:18 -0300

    Image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+const imageBg = './assets/images/background-galaxy.png';
 module.exports = {
   content: ['./src/**/*.tsx', './index.html'],
   theme: {
@@ -9,7 +11,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         // prettier-ignore
-        galaxy: 'url(\'./assets/images/background-galaxy.png\')',
+        galaxy: `url(${imageBg})`,
         'nlw-gradient':
           'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 40.57%)',
         'game-gradient':
